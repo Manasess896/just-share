5974ec859f895d9ffe3a9c2a5839efa134b87192
manasess896 <manasesskamau1053@gmail.com>
2026-01-05 17:33:50 +0300

    Add navigation bar to gallery, index, and view pages

--- view.php
+++ view.php
@@ -390,6 +390,27 @@ $previewUrl = ($isImage && $isUnlocked) ? $downloadUrl . "?preview=1" : "";
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-custom mb-4">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="<?php echo $config['base_url']; ?>/home">
+                <img src="<?php echo $config['base_url']; ?>/logo.jpeg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top me-2 rounded-circle">
+                Just Share
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo $config['base_url']; ?>/home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo $config['base_url']; ?>/files">Gallery</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
 
     <div class="container">
